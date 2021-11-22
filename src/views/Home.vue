9bc342228e4880467ed8a6a4cdf43294c3fab25c
Brandy McKinney <missearlg80@gmail.com>
2021-11-21 20:13:50 -0500

    Show Vue.js repo data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,43 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    "Stargazers GitHub Repo Project"
+    <!-- <h1>{{ message }}</h1> -->
+    <h1>Vuejs Repo</h1>
+    <div v-for="info in repoData" v-bind:key="info.id">
+      {{ info.forks_count }}
+      {{ info.watchers }}
+      {{ info.stargazers_count }}
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+<style></style>
 
+<script>
+import axios from "axios";
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
+  data: function () {
+    return {
+      message: "Hi!",
+      repoData: [],
+    };
+  },
+  created: function () {
+    this.getRepo();
+  },
+  methods: {
+    getRepo: function () {
+      axios
+        .get("https://api.github.com/repos/vuejs/vue")
+        .then((response) => {
+          this.repoData = [...this.repoData, response.data];
+          console.log("All Vuejs Repo data", this.repoData);
+        })
+        .catch((error) => {
+          console.log(error.response);
+          this.repoData = {};
+        });
+    },
   },
 };
 </script>
